a7ab25be4b82790a42b176636ed9e7eb6f4950cb
The-Forecaster <DarkDruid11@gmail.com>
2022-11-27 23:49:32 -0700

    initial commit

---
+++ target/debug/deps/renderer-691394a8261c1e5f.d
@@ -0,0 +1,5 @@
+c:\Users\DarkD\Desktop\renderer\target\debug\deps\renderer-691394a8261c1e5f.rmeta: src\main.rs
+
+c:\Users\DarkD\Desktop\renderer\target\debug\deps\renderer-691394a8261c1e5f.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/renderer-d29d76591f8861d2.d
@@ -0,0 +1,5 @@
+c:\Users\DarkD\Desktop\renderer\target\debug\deps\renderer-d29d76591f8861d2.rmeta: src\main.rs
+
+c:\Users\DarkD\Desktop\renderer\target\debug\deps\renderer-d29d76591f8861d2.d: src\main.rs
+
+src\main.rs:
