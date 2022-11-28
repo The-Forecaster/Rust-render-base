cacc116fa7eceb625a100d04de0374f4871110d0
The-Forecaster <DarkDruid11@gmail.com>
2022-11-28 00:03:33 -0700

    last commit ima go to bed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Coordinate {
+    x: u32,
+    y: u32
+}
+
+struct Quad {
+    x1: Coordinate,
+    x2: Coordinate,
+    y1: Coordinate,
+    y2: Coordinate
+}
+
+struct Cube {
+    start: Coordinate,
+    length: u32,
+    width: u32
+}
+
