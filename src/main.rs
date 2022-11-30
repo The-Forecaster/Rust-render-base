b54c4694b55cb5be8d516e2d168790d227abc1da
The-Forecaster <DarkDruid11@gmail.com>
2022-11-30 11:33:41 -0700

    started some more render stuff and made a cube

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,47 @@
+use polygon3d::{Polygon3D, Surface3D};
+
+mod polygon3d;
+
 fn main() {
-    println!("Hello, world!");
-}
+    let cube = Polygon3D::new(
+        vec![
+                Surface3D::new(vec![
+                    (0, 0, 0),
+                    (100, 0, 0),
+                    (0, 100, 0),
+                    (100, 100, 0)
+                ]),
+                Surface3D::new(vec![
+                    (0, 0, 0),
+                    (0, 100, 0),
+                    (0, 0, 100),
+                    (0, 100, 100)
+                ]),
+                Surface3D::new(vec![
+                    (0, 0, 0),
+                    (0, 100, 0),
+                    (0, 0, 100),
+                    (0, 100, 100)
+                ]),
+                Surface3D::new(vec![
+                    (100, 0, 0),
+                    (100, 100, 0),
+                    (100, 0, 100),
+                    (100, 100, 100)
+                ]),
+                Surface3D::new(vec![
+                    (0, 100, 0),
+                    (100, 100, 0),
+                    (0, 100, 100),
+                    (100, 100, 100)
+                ]),
+                Surface3D::new(vec![
+                    (0, 0, 100),
+                    (100, 0, 100),
+                    (0, 100, 100),
+                    (100, 100, 100)
+                ])
+            ]);
 
-struct Coordinate {
-    x: u16,
-    y: u16
-}
-
-impl Coordinate {
-    fn new(x: u16, y: u16) -> Self {
-        Self { x, y }
-    }
-}
-
-struct Quad {
-    x: Coordinate,
-    y: Coordinate,
-    z: Coordinate,
-    a: Coordinate
-}
-
-impl Quad {
-    fn new(x: Coordinate, y: Coordinate, z: Coordinate, a: Coordinate) -> Self {
-        Self { x, y, z, a }
-    }
-
-    fn rect(start: Coordinate, width: u16, height: u16) -> Self {
-        Self { x: Coordinate::new(start.x, start.y), y: Coordinate::new(start.x + height, start.y), z: Coordinate::new(start.x, start.y + width), a: Coordinate::new(&start.x + height, start.y + width)}
-    }
-}
-
-struct Square {
-    start: Coordinate,
-    length: u16
+    cube.draw()
 }
