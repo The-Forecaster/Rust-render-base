ced5b0f0f1dfbfef187b7618cf017a70a79f424d
The-Forecaster <DarkDruid11@gmail.com>
2023-01-30 14:07:54 -0700

    stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub type Point = [u16; 2]; // point should be ordered x, y
 
 pub type Quad = [Point; 4]; // point 1, point 2, point 3, point 4
+
+pub type Rect = (Point, [u16; 2]); // Top left, width, height
